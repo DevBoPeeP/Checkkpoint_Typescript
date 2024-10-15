aae6075d2c6a96f2233b9ba16c6cfbe5b295678e
Pen04guin <idunchiamaka@gmail.com>
2024-10-15 17:55:07 +0100

    checkpoint updated

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+class Car {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    start() {
+        console.log("Car engine started");
+    }
+}
+const myCar = new Car("Toyota", "Camry", 2020);
+myCar.start(); // This should log "Car engine started" to the console
